8a4e9a19b3266503488c73cd8644edd51749be21
perlancar (@pc-office) <perlancar@gmail.com>
2015-09-16 18:48:59 +0700

    Initial commit

---
+++ lib/Complete/Dpkg.pm
@@ -0,0 +1,34 @@
+package Complete::Dpkg;
+
+# DATE
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+use Complete::Setting;
+
+require Exporter;
+our @ISA = qw(Exporter);
+our @EXPORT_OK = qw(
+                       complete_dpkg
+               );
+
+our %SPEC;
+
+1;
+# ABSTRACT: Completion routines related to Debian packages
+
+=for Pod::Coverage .+
+
+=head1 DESCRIPTION
+
+B<NAME GRAB. NOT YET IMPLEMENTED.>
+
+
+=head1 SEE ALSO
+
+L<Complete>
+
+Other C<Complete::*> modules.
